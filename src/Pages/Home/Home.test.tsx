abf02b446ba872d64e1592dd8a80b2454a06d465
Juan Guillermo Murillo <jg.murillo10@uniandes.edu.co>
2021-12-31 16:17:07 -0500

    Fix | Test after react-router implementatioin

--- src/Pages/Home/Home.test.tsx
+++ src/Pages/Home/Home.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './index';
+import App from './../../App';
 
 test('renders title element', () => {
   render(<App />);
-  const exploreButton = screen.getByText(/Explore trending/i);
+  const exploreButton = screen.getByText(/Let's cook something together/i);
   expect(exploreButton).toBeInTheDocument();
 });
